bf90ca0f9267dd9b38ebf3a43b15e993e14660c1
Arlen Cuss <celtic@sairyx.org>
2009-03-15 23:55:24 +1100

    longer names unnecessary

--- inc/AkariDescriptorSubsystem.hpp
+++ inc/AkariDescriptorSubsystem.hpp
@@ -5,28 +5,6 @@
 
 // GDT structures
 
-extern "C" struct GDTEntry {
-	u16 limit_low;
-	u16 base_low;
-	u8 base_middle;
-	union {
-		u8 access;
-		struct {
-			unsigned segment_type : 4;
-			unsigned descriptor_type : 1;
-			unsigned privilege_level : 2;
-			unsigned present : 1;
-		} __attribute__((__packed__));
-	};
-	u8 granularity;
-	u8 base_high;
-} __attribute__((__packed__));
-
-extern "C" struct GDTPointer {
-	u16 limit;
-	u32 base;
-} __attribute__((__packed__));
-
 // the subsystem itself
 
 class AkariDescriptorSubsystem : public AkariSubsystem {
@@ -44,8 +22,30 @@ class AkariDescriptorSubsystem : public AkariSubsystem {
 				void SetGate(s32, u32, u32, u8, u8);
 
 			protected:
-				GDTEntry *_entries;
-				GDTPointer *_pointer;
+				struct Entry {
+					u16 limit_low;
+					u16 base_low;
+					u8 base_middle;
+					union {
+						u8 access;
+						struct {
+							unsigned segment_type : 4;
+							unsigned descriptor_type : 1;
+							unsigned privilege_level : 2;
+							unsigned present : 1;
+						} __attribute__((__packed__));
+					};
+					u8 granularity;
+					u8 base_high;
+				} __attribute__((__packed__));
+
+				struct Pointer {
+					u16 limit;
+					u32 base;
+				} __attribute__((__packed__));
+
+				Entry *_entries;
+				Pointer *_pointer;
 		};
 
 		GDT _gdt;
