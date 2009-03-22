642dc77b5002cafb031f17ec041c19eba3e45968
Arlen Cuss <celtic@sairyx.org>
2009-03-23 00:55:01 +1100

    relocating the stack to 0xE000,0000. yay! :)

--- entry.cpp
+++ entry.cpp
@@ -14,6 +14,16 @@ void timer_func(struct registers r) {
 	// Akari->Console->PutString("timer run\n");
 }
 
+void NewStackTest() {
+	u32 abc = 0xbadbeef;
+	Akari->Console->PutString("addr of abc: ");
+	Akari->Console->PutInt((u32)&abc, 16);
+	Akari->Console->PutString("\n");
+	Akari->Console->PutString("content of abc: ");
+	Akari->Console->PutInt(abc, 16);
+	Akari->Console->PutString("\n");
+}
+
 void AkariEntry() {
 	if ((AkariMultiboot->flags & 0x41) != 0x41)
 		AkariPanic("Akari: MULTIBOOT hasn't given us enough information about memory.");
@@ -35,11 +45,23 @@ void AkariEntry() {
 	
 	// here we switch the stack to somewhere predictable.. assume we don't need
 	// any of the stack as it is
-	// AkariSetupStack(KERNEL_STACK_POS, KERNEL_STACK_SIZE);	// this returns manually, so be careful
 
 	u32 phys;
 	void *xyz = Akari->Memory->Alloc(10, &phys);
-	
+	Akari->Console->PutString("\nallocated to ");
+	Akari->Console->PutInt((u32)xyz, 16);
+	Akari->Console->PutString(", phys ");
+	Akari->Console->PutInt(phys, 16);
+	Akari->Console->PutString("\n");
+
+	AkariSetupStack(KERNEL_STACK_POS, KERNEL_STACK_SIZE);
+	__asm__ __volatile__("AkariEntryNewStack:");
+	// Note that we cannot `make' new stack variables here, as they're actually
+	// all part of the same stack which the C++ compiler thinks we had all along.
+	// (imagine they're initialised at the start of the function).
+	// Function calls work fine, though, as they all push onto the current stack.
+
+	NewStackTest();
 
 	Akari->Console->PutString("\nSystem halted!");
 	asm volatile("sti");
@@ -48,6 +70,8 @@ void AkariEntry() {
 }
 
 void AkariSetupStack(u32 start, u32 size) {
+	// NOTE: this function returns to AkariEntryNewStack directly.
+
 	for (u32 i = start; i >= start - size; i -= 0x1000)
 		Akari->Memory->_activeDirectory->GetPage(i, true)->AllocAnyFrame(false, true);
 	
@@ -56,15 +80,9 @@ void AkariSetupStack(u32 start, u32 size) {
 		mov %%cr3, %%eax; \
 		mov %%eax, %%cr3" : : : "%eax");
 	
-	u32 old_esp, old_ebp;
 	__asm__ __volatile__("\
-		mov %%esp, %0; \
-		mov %%ebp, %1" : "=r" (old_esp), "=r" (old_ebp));
-
-	Akari->Console->PutString("ESP: ");
-	Akari->Console->PutInt(old_esp, 16);
-	Akari->Console->PutString("\nEBP: ");
-	Akari->Console->PutInt(old_ebp, 16);
-	Akari->Console->PutString("\n");
+		mov %%eax, %%esp; \
+		mov %%eax, %%ebp; \
+		jmp AkariEntryNewStack" : : "a" (start));
 }
 
