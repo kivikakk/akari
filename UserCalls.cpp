95e7a065824eedbc33c4da076cd8159a785b0d21
Arlen Cuss <celtic@sairyx.org>
2009-05-15 23:38:46 +1000

    Using a dirty hack that must be removed soon to get blocking to work, as such.

--- UserCalls.cpp
+++ UserCalls.cpp
@@ -16,7 +16,13 @@ namespace User {
 	void IrqWait(u32 irq) {
 		Akari->Task->current->irqWait = irq;
 		Akari->Console->PutString("[<] ");
-		irq0();	// do what the timer does. seems a fairly cheap (as in hackish) way to do things, though. スタックとか大丈夫かな
+
+		// irq0();	// do what the timer does. seems a fairly cheap (as in hackish) way to do things, though. スタックとか大丈夫かな
+		// the above blatently doesn't work. hm.
+
+		// this below code DOES, but isn't it the dirtiest hack ever?! also slow, since we don't return immediately.
+		__asm__ __volatile__("sti; hlt");
+
 		Akari->Console->PutString("[>] ");
 		Akari->Task->current->irqWait = 0;
 	}
