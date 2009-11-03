93fc94f0aad7c206c0e270fc42e3dfc154014769
Arlen Cuss <celtic@tsunagari.(none)>
2009-11-03 16:39:40 +1100

    Here's an environment for Linux users

---
+++ tools/linux_environment.sh
@@ -0,0 +1,8 @@
+export AS=as
+export LD=ld
+export CC=gcc
+export CXX=g++
+export STRIP=strip
+export MTOOLS_BIN=/usr/bin
+alias qemu="qemu -m 128 -localtime -net nic -net user -fda /home/celtic/Code/akari/disk.img -hda /home/celtic/Code/akari/16disk.img -cdrom /dev/cdrom -boot a"
+alias debug="gdb Akari -ex 'target remote localhost:1234'"
