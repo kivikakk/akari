c6f6f644764a29db8fefe546010dc387e97fe7b4
Arlen Cuss <celtic@sairyx.org>
2010-01-30 02:45:16 +1100

    Now using mindrvr for everything! Next up: DMA.

--- packages/ATA/main.cpp
+++ packages/ATA/main.cpp
@@ -32,16 +32,12 @@ void ata_read_sectors(u32 start, u32 number, u8 *buffer);
 void ata_write_sectors(u32 start, u32 number, u8 *buffer);
 
 extern "C" int main() {
-	printf("reg_config start\n");
 	int devices_found = reg_config();
-	printf("reg_config complete\n");
 
 	if (devices_found == 0) {
 		printf("ATA: failed init - totally no hard drives\n");
 		return 0;
 	}
-
-	printf("found %d device(s)\n", devices_found);
 
 	// Now we need to wait and listen for commands!
 	if (!registerName("system.io.ata"))
@@ -78,48 +74,40 @@ extern "C" int main() {
 }
 
 void ata_read_sectors(u32 start, u32 number, u8 *buffer) {
+	/* I'm not sure if the code below will do what's anticipated of it.
+	 * sc will be clipped to 8 bits. Could mean nasty stuff, seeing as
+	 * we're banking on the default "0 means 256", but you know, 3 means 3.
+	 *
+	 * e.g. If we transfer 259 sectors, (u8)259==(u8)3, so a call like:
+	 * reg_pio_data_in_lba28(0, CMD_READ_SECTORS, 0, 259, start, buf, 259, 0)
+	 * .. will eventually set sc to 3 in the command.
+	 * This next bit here might buffer some of the impact by limiting our
+	 * calls to mindrvr to doing 256 sectors at a time.
+	 */
+
+	// Definitive answer:
+	//   I've concluded that, seeing as sc is passed straight through to
+	// the sector count port, we need to manage its value ourself.
+	//   number > 256 in the reg_pio_data_in_lba28 call means sc>256, but
+	// it gets truncated to a u8 and thus havoc ensures trying to read in
+	// 1025 sectors when each read call gives you 1 and not the 256 you
+	// were hoping for, save the last time.
+	//   mindrvr doesn't mitigate this situation for us, since the actual
+	// reg_pio_data_in_lba28 call can be used with multiCnt>1 and a cmd
+	// like CMD_READ_MULTIPLE - i.e. the caller is expected to know ATA.
+	// (otherwise we wouldn't have to pass in a cmd!)
+
 	while (number > 256) {
 		ata_read_sectors(start, 256, buffer);
 		buffer += 512 * 256;
 		start += 256;
 		number -= 256;
 	}
-	
-	AkariOutB(ATA_PRIMARY + ATA_DRIVE, ATA_SELECT_MASTER_OP | ((start >> 24) & 0x0F));
 
-	// replace with more intelligence later
-	AkariInB(ATA_PRIMARY + ATA_DCR);
-	AkariInB(ATA_PRIMARY + ATA_DCR);
-	AkariInB(ATA_PRIMARY + ATA_DCR);
-	AkariInB(ATA_PRIMARY + ATA_DCR);	
-	AkariOutB(ATA_PRIMARY + ATA_FEATURES, 0x00);
-	AkariOutB(ATA_PRIMARY + ATA_SECTOR, (number == 256) ? 0 : number);	// sector count
-	AkariOutB(ATA_PRIMARY + ATA_PDSA1, static_cast<u8>(start & 0xFF));	// low 8 bits of LBA
-	AkariOutB(ATA_PRIMARY + ATA_PDSA2, static_cast<u8>((start >> 8) & 0xFF));	// next 8
-	AkariOutB(ATA_PRIMARY + ATA_PDSA3, static_cast<u8>((start >> 16) & 0xFF));	// next 8
-	AkariOutB(ATA_PRIMARY + ATA_CMD, ATA_READ_SECTORS);
-
-	u32 sectors_read = 0;
-	while (sectors_read < number) {
-		u8 rs = AkariInB(ATA_PRIMARY + ATA_CMD);
-		while (!(!(rs & ATA_BSY) && (rs & ATA_DRQ)) && !(rs & ATA_ERR) && !(rs & ATA_DF))
-			rs = AkariInB(ATA_PRIMARY + ATA_CMD);
-		
-		if (rs & ATA_ERR) 	panic("ATA: ATA_ERR in ata_read_sectors\n");
-		else if (rs & ATA_DF) 	panic("ATA: ATA_DF in ata_read_sectors\n");
-
-		for (u32 i = 0; i < 256; ++i) {
-			u16 incoming = AkariInW(ATA_PRIMARY + ATA_DATA);
-			*buffer++ = incoming & 0xff;
-			*buffer++ = (incoming >> 8) & 0xff;
-		}
-
-		sectors_read++;
-	}
+	reg_pio_data_in_lba28(0, CMD_READ_SECTORS, 0, number, start, buffer, number, 0);
 }
 
-void ata_read_data(u32 sector_offset, u16 offset, u32 length, u8 *buffer)
-{
+void ata_read_data(u32 sector_offset, u16 offset, u32 length, u8 *buffer) {
 	static u8 tempdata[512];
 
 	// just in case some idiot passes in offset >= 512. why?! (that'd probably be me ..)
@@ -129,15 +117,19 @@ void ata_read_data(u32 sector_offset, u16 offset, u32 length, u8 *buffer)
 	u32 sectors_read = 0;
 
 	if (offset > 0) {
+		// Since the start of our read isn't on a sector boundary, we need to grab
+		// that whole sector, then just copy starting from the relevant position to
+		// our buffer.
 		ata_read_sectors(sector_open, 1, static_cast<u8 *>(tempdata));
-		memcpy(buffer, tempdata + offset, min(static_cast<u32>(512 - offset), length));	// XXX: what was this comment about!?	/* the +1 is important, but i don't understand why */
-		buffer += min(static_cast<u32>(512 - offset), length);										// XXX: and this??/* yet no +1 here */
+		memcpy(buffer, tempdata + offset, min(static_cast<u32>(512 - offset), length));
+		buffer += min(static_cast<u32>(512 - offset), length);
 		++sectors_read;
 		length -= min(512, offset + length) - offset;
 	}
 	
 	u32 complete_sectors = length / 512;
 	if (complete_sectors > 0) {
+		// We have some number of complete sectors which we can read out wholesale.
 		ata_read_sectors(sector_open + sectors_read, complete_sectors, buffer);
 		sectors_read += complete_sectors;
 		buffer += complete_sectors * 512;
@@ -145,6 +137,10 @@ void ata_read_data(u32 sector_offset, u16 offset, u32 length, u8 *buffer)
 	length %= 512;
 
 	if (length > 0) {
+		// There's some data left over which don't make up a whole sector. Read the
+		// last sector in, then copy over the appropriate portion to our buffer.
+		// We don't update our counters (as we do in the start-case) as we don't
+		// need them again.
 		ata_read_sectors(sector_open + sectors_read, 1, static_cast<u8 *>(tempdata));
 		memcpy(buffer, tempdata, length);
 		// buffer, sectors_read, length stale
@@ -152,8 +148,7 @@ void ata_read_data(u32 sector_offset, u16 offset, u32 length, u8 *buffer)
 }
 
 
-void ata_write_data(u32 sector_offset, u16 offset, u32 length, u8 *buffer)
-{
+void ata_write_data(u32 sector_offset, u16 offset, u32 length, u8 *buffer) {
 	static u8 tempdata[512];
 
 	u32 sector_open, sectors_written = 0;
@@ -163,6 +158,10 @@ void ata_write_data(u32 sector_offset, u16 offset, u32 length, u8 *buffer)
 	offset %= 512;
 
 	if (offset > 0) {
+		// Since we're not starting our data write at a sector-aligned place,
+		// we have to read in the sector, then copy our change over the portion
+		// of it that's different, then write out that whole sector.
+
 		ata_read_sectors(sector_open, 1, static_cast<u8 *>(tempdata));
 		memcpy(tempdata + offset, buffer, min(static_cast<u32>(512 - offset), length));
 		ata_write_sectors(sector_open, 1, static_cast<u8 *>(tempdata));
@@ -173,6 +172,8 @@ void ata_write_data(u32 sector_offset, u16 offset, u32 length, u8 *buffer)
 
 	complete_sectors = length / 512;
 	if (complete_sectors > 0) {
+		// We have complete sectors which can be written out without any
+		// funny business.
 		ata_write_sectors(sector_open + sectors_written, complete_sectors, buffer);
 		sectors_written += complete_sectors;
 		buffer += complete_sectors * 512;
@@ -180,54 +181,25 @@ void ata_write_data(u32 sector_offset, u16 offset, u32 length, u8 *buffer)
 	length %= 512;
 
 	if (length > 0) {
+		// Grab the last sector, put our modifications on top, rewrite (as above).
+		// We don't update our counters like we do last time, since we don't need
+		// them again.
 		ata_read_sectors(sector_open + sectors_written, 1, static_cast<u8 *>(tempdata));
 		memcpy(tempdata, buffer, length);
 		ata_write_sectors(sector_open + sectors_written, 1, static_cast<u8 *>(tempdata));
-		/* buffer, sectors_written, length stale */
+		// buffer, sectors_written, length stale
 	}
 }
 
-void ata_write_sectors(u32 start, u32 number, u8 *buffer)
-{
-	u8 rs;
-	u32 i, sectors_written = 0;
-
-	if (number > 256)
-		panic("ATA: we haven't implemented writing more than 256 sectors at once yet (> 128KiB)\n");
-	else if (number == 0)
-		panic("ATA: writing 0 sectors?\n");
-
-	AkariOutB(ATA_PRIMARY + ATA_DRIVE, ATA_SELECT_MASTER_OP | ((start >> 24) & 0x0F));
-	AkariInB(ATA_PRIMARY + ATA_DCR);
-	AkariInB(ATA_PRIMARY + ATA_DCR);
-	AkariInB(ATA_PRIMARY + ATA_DCR);
-	AkariInB(ATA_PRIMARY + ATA_DCR);	/* see above */
-	AkariOutB(ATA_PRIMARY + ATA_FEATURES, 0x00);
-	AkariOutB(ATA_PRIMARY + ATA_SECTOR, (number == 256) ? 0 : number);
-	AkariOutB(ATA_PRIMARY + ATA_PDSA1, static_cast<u8>(start & 0xFF));
-	AkariOutB(ATA_PRIMARY + ATA_PDSA2, static_cast<u8>((start >> 8) & 0xFF));
-	AkariOutB(ATA_PRIMARY + ATA_PDSA3, static_cast<u8>((start >> 16) & 0xFF));
-	AkariOutB(ATA_PRIMARY + ATA_CMD, ATA_WRITE_SECTORS);
-
-	while (sectors_written < number) {
-		rs = AkariInB(ATA_PRIMARY + ATA_CMD);
-		while (!(!(rs & ATA_BSY) && (rs & ATA_DRQ)) && !(rs & ATA_ERR) && !(rs & ATA_DF))
-			rs = AkariInB(ATA_PRIMARY + ATA_CMD);
-		
-		if (rs & ATA_ERR) 	panic("ATA: ATA_ERR in ata_write_sectors\n");
-		else if (rs & ATA_DF) 	panic("ATA: ATA_DF in ata_write_sectors\n");
-
-		for (i = 0; i < 256; ++i) {
-			AkariOutW(ATA_PRIMARY + ATA_DATA, buffer[0] | (buffer[1] << 8));
-			buffer += 2;
-			asm volatile("jmp .+2");
-		}
-
-		sectors_written += 1;
+void ata_write_sectors(u32 start, u32 number, u8 *buffer) {
+	// See ata_read_sectors for a big rant on mindrvr usage.
+	
+	while (number > 256) {
+		ata_write_sectors(start, 256, buffer);
+		buffer += 512 * 256;
+		start += 256;
+		number -= 256;
 	}
 
-	AkariOutB(ATA_PRIMARY + ATA_CMD, ATA_CACHE_FLUSH);
-	rs = AkariInB(ATA_PRIMARY + ATA_CMD);
-	while ((rs & ATA_BSY))
-		rs = AkariInB(ATA_PRIMARY + ATA_CMD);
+	reg_pio_data_out_lba28(0, CMD_WRITE_SECTORS, 0, number, start, buffer, number, 0);
 }
