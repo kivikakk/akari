f3b1bbb4e6d95cbb6f65e5ce3dfafc2fc40e78fe
Arlen Cuss <celtic@sairyx.org>
2010-07-06 18:48:16 +1000

    Using a different image

--- tools/linux_environment.sh
+++ tools/linux_environment.sh
@@ -19,5 +19,5 @@ export CC=gcc
 export CXX=g++
 export STRIP=strip
 export MTOOLS_BIN=/usr/bin
-alias qemu="qemu -m 128 -localtime -net nic -net user -hda /home/celtic/Code/akari/32disk.img -cdrom /dev/cdrom -boot c"
+alias qemu="qemu -m 128 -localtime -net nic -net user -hda /home/celtic/Code/akimg.img -cdrom /dev/cdrom -boot c"
 alias debug="gdb Akari -ex 'target remote localhost:1234'"
